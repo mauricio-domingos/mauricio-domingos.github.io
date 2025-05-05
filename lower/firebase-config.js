818a1ee9f2f2babf8d493e18519eab74dd57ef41
mauricio-domingos <tg59542@gmail.com>
2025-05-05 15:45:50 -0300

    lower

---
+++ lower/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyA9XsiuV-BLF8kV-2_xg9y7rzRqoIUdPDA",
+    authDomain: "tvarublox.firebaseapp.com",
+    databaseURL: "https://tvarublox-default-rtdb.firebaseio.com",
+    projectId: "tvarublox",
+  };
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  
